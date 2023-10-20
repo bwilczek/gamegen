e866f30153bb1ca7820292885f72e970def2f7f6
Bartek Wilczek <bartek.wilczek@toptal.com>
2023-10-20 11:21:51 +0200

    WiP: parsing logical expressions

--- lib/gamegen/syntax/condition/parser.rb
+++ lib/gamegen/syntax/condition/parser.rb
@@ -10,6 +10,9 @@ module Gamegen
 
         rule(:space) { match('\s').repeat(1) }
         rule(:space?) { space.maybe }
+        rule(:lparen) { str('(') >> space? }
+        rule(:rparen) { str(')') >> space? }
+
         rule(:bool) { (str('true') | str('false')).as(:bool) >> space? }
         rule(:integer) { match('[0-9]').repeat(1).as(:int) >> space? }
         rule(:identifier) { (match('[a-zA-Z=*]') >> match('[a-z0-9A-Z=*_]').repeat).as(:identifier) >> space? }
@@ -19,12 +22,19 @@ module Gamegen
         rule(:op_gte) { str('>=') }
         rule(:op_gt) { str('>') }
         rule(:op_lt) { str('<') }
-        rule(:operator) { (op_eq | op_neq | op_gte | op_lte | op_lt | op_gt).as(:operator) >> space? }
+        rule(:cmp_operator) { (op_eq | op_neq | op_gte | op_lte | op_lt | op_gt).as(:operator) >> space? }
         rule(:value) { integer | bool | identifier }
 
-        rule(:basic_condition) { identifier.as(:left) >> operator >> value.as(:right) }
+        rule(:op_and) { str('&&') }
+        rule(:op_or) { str('||') }
+        rule(:logical_operator) { (op_and | op_or).as(:operator) >> space? }
 
-        rule(:condition) { bool | basic_condition }
+        rule(:basic_condition) { identifier.as(:left) >> cmp_operator >> value.as(:right) }
+        rule(:condition_in_parens) { lparen >> basic_condition >> rparen }
+
+        rule(:logical_operation) { condition_in_parens.as(:left) >> logical_operator >> condition_in_parens.as(:right) }
+
+        rule(:condition) { logical_operation | bool | basic_condition | condition_in_parens }
         root(:condition)
       end
     end
--- spec/gamegen/syntax/condition/parser_spec.rb
+++ spec/gamegen/syntax/condition/parser_spec.rb
@@ -11,17 +11,39 @@ RSpec.describe(Gamegen::Syntax::Condition::Parser) do
     describe 'basic expression' do
       let(:input) { 'strength >= 8' }
 
-      specify do
-        binding.pry
+      specify do # rubocop:disable RSpec/MultipleExpectations
+        expect(result.dig(:left, :identifier)).to eq('strength')
+        expect(result[:operator]).to eq('>=')
+        expect(result.dig(:right, :int)).to eq('8')
       end
     end
 
-    describe 'complete expression' do
-      let(:input) { '(strength >= 8) || ((character != mage) && (money == 20))' }
+    describe 'expression in parens' do
+      let(:input) { '(strength >= 8)' }
 
-      specify do
-        # binding.pry
+      specify do # rubocop:disable RSpec/MultipleExpectations
+        expect(result.dig(:left, :identifier)).to eq('strength')
+        expect(result[:operator]).to eq('>=')
+        expect(result.dig(:right, :int)).to eq('8')
       end
     end
+
+    describe 'logical witout nesting parens' do
+      let(:input) { '(strength >= 8) && (money < 3)' }
+
+      specify do # rubocop:disable RSpec/MultipleExpectations
+        expect(result.dig(:left, :operator)).to eq('>=')
+        expect(result[:operator]).to eq('&&')
+        expect(result.dig(:right, :operator)).to eq('<')
+      end
+    end
+
+    # describe 'complete expression' do
+    #   let(:input) { '(strength >= 8) || ((character != mage) && (money == 20))' }
+
+    #   specify do
+    #     expect(result.dig(:right, :operator)).to eq('&&')
+    #   end
+    # end
   end
 end
